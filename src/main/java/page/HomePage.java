5ddc2f54a9e348ef8983d2338336a6b339d83231
Mits16 <mits1388@gmail.com>
2024-10-19 22:17:20 +0300

    add class home page

---
+++ src/main/java/page/HomePage.java
@@ -0,0 +1,18 @@
+package page;
+
+public class HomePage extends BasePage {
+    protected String HOME_URL = BASE_URL + "Mits1388";
+    String title = "Домашняя страница";
+
+    public void open() {
+        System.out.println("Пользователь открыл страницу " + HOME_URL);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void clickLogin() {
+        System.out.println("Пользователь кликнул по кнопке Войти");
+    }
+}
