2b92c850fdaeca3c423ee26f677dab0b9af1e52f
Mits16 <mits1388@gmail.com>
2024-10-19 22:18:02 +0300

    add class main

---
+++ src/main/java/page/Main.java
@@ -0,0 +1,18 @@
+package page;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BasePage basePage = new BasePage();
+        HomePage homePage = new HomePage();
+        LoginPage loginPage = new LoginPage();
+
+        basePage.open();
+        homePage.open();
+        System.out.println(homePage.getTitle());
+        homePage.clickLogin();
+        loginPage.open();
+        System.out.println(loginPage.getTitle());
+    }
+}
