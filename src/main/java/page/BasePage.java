342392044e69fdc5fbaea24739481a26ee3553c1
Mits16 <mits1388@gmail.com>
2024-10-19 22:17:07 +0300

    add class base page

---
+++ src/main/java/page/BasePage.java
@@ -0,0 +1,9 @@
+package page;
+
+public class BasePage {
+    protected String BASE_URL = "https://github.com/";
+
+    public void open() {
+        System.out.println("Пользователь открыл страницу " + BASE_URL);
+    }
+}
