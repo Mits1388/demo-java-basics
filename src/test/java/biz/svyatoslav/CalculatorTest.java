cb9950f8d558658684bb58598a4d5a1aac518a5d
Mits16 <mits1388@gmail.com>
2024-10-22 22:21:20 +0300

    add gender

--- src/test/java/biz/svyatoslav/CalculatorTest.java
+++ src/test/java/biz/svyatoslav/CalculatorTest.java
@@ -25,6 +25,7 @@ public class CalculatorTest {
     public void test2(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
         String inputNameXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
         By inputNameBy = By.xpath(inputNameXpath);
         WebElement inputNameWebElement = driver.findElement(inputNameBy);
@@ -40,14 +41,40 @@ public class CalculatorTest {
         WebElement inputWeightWebElement = driver.findElement(inputWeightBy);
         inputWeightWebElement.sendKeys("80");
 
+        String buttonXpath  = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By buttonBy = By.xpath(buttonXpath);
+        WebElement buttonWebElement = driver.findElement(buttonBy);
+        buttonWebElement.click();
+
+    }
+    @Test
+    public void testHeight(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        String inputNameXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNameBy = By.xpath(inputNameXpath);
+        WebElement inputNameWebElement = driver.findElement(inputNameBy);
+        inputNameWebElement.sendKeys("John");
+
+        String inputHeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By inputHeightBy = By.xpath(inputHeightXpath);
+        WebElement inputHeightWebElement = driver.findElement(inputHeightBy);
+        inputHeightWebElement.sendKeys("180");
+
+        String inputWeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By inputWeightBy = By.xpath(inputWeightXpath);
+        WebElement inputWeightWebElement = driver.findElement(inputWeightBy);
+        inputWeightWebElement.sendKeys("80");
+
+        String genderXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
+        By genderBy = By.xpath(genderXpath);
+        WebElement genderWebElement = driver.findElement(genderBy);
+        genderWebElement.click();
 
         String buttonXpath  = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
         By buttonBy = By.xpath(buttonXpath);
         WebElement buttonWebElement = driver.findElement(buttonBy);
         buttonWebElement.click();
-    }
-    @Test
-    public void testHeight(){
-
     }
 }
