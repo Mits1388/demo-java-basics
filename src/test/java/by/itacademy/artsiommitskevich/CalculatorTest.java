0f6f76b0272a38f00ea7a5ac9a0b3826c9f02bb8
Mits16 <mits1388@gmail.com>
2024-10-22 21:45:55 +0300

    refactor method testClick

--- src/test/java/by/itacademy/artsiommitskevich/CalculatorTest.java
+++ src/test/java/by/itacademy/artsiommitskevich/CalculatorTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CalculatorTest {
 
     @Test
-    public void testSite1(){
+    public void testClick(){
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
         String xPathButton = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
